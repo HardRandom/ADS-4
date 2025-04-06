fa48c9d1ee1aececea114470c8b79d05850d58d8
HardRandom <90847034+HardRandom@users.noreply.github.com>
2025-04-06 17:40:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,39 +11,38 @@ int countPairs1(int *arr, int len, int value) {
     return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  int left = 0;
-  int right = len - 1;
-  int count = 0;
-
-  while (left < right) {
-       int sum = arr[left] + arr[right];
-      if (sum == value) {
-          if (arr[left] == arr[right]) {
-              int total = right - left + 1;
-               count += total * (total - 1) / 2;
-               break;
-           } else {
-               int count_left = 1;
-               while (left + 1 < right && arr[left + 1] == arr[left]) {
-                   ++left;
-                  ++count_left;
-               }
-              int count_right = 1;
-              while (right - 1 > left && arr[right - 1] == arr[right]) {
-                  --right;
-                  ++count_right;
-              }
-              count += count_left * count_right;
-               ++left;
-              --right;
-           }
-      } else if (sum < value) {
-          ++left;
-      } else {
-          --right;
-      }
-   }
-  return count;
+    int left = 0;
+    int right = len - 1;
+    int count = 0;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == value) {
+            if (arr[left] == arr[right]) {
+                int total = right - left + 1;
+                count += total * (total - 1) / 2;
+                break;
+            } else {
+                int count_left = 1;
+                while (left + 1 < right && arr[left + 1] == arr[left]) {
+                    ++left;
+                    ++count_left;
+                }
+                int count_right = 1;
+                while (right - 1 > left && arr[right - 1] == arr[right]) {
+                    --right;
+                    ++count_right;
+                }
+                count += count_left * count_right;
+                ++left;
+                --right;
+            }
+        } else if (sum < value) {
+            ++left;
+        } else {
+            --right;
+        }
+    }
+    return count;
 }
 int binarySearch(int *arr, int len, int target, int exclude_index) {
     int result = -1;
