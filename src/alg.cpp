317cf4169ec3e1021e28964f7ae184fb179132b8
HardRandom <90847034+HardRandom@users.noreply.github.com>
2025-04-06 18:11:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,43 +11,22 @@ int countPairs1(int *arr, int len, int value) {
     return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  int left = 0;
-    int right = len - 1;
-    int count = 0;
-    
-    while (left < right) {  // Изменено с <= на <
-        int sum = arr[left] + arr[right];
-        
-        if (sum == value) {
-            if (arr[left] == arr[right]) {
-                int k = right - left + 1;
-                count += k * (k - 1) / 2;
-                break;
-            }
-            
-            int cnt_left = 1;
-            while (left + cnt_left < len && arr[left + cnt_left] == arr[left]) {
-                cnt_left++;
-            }
-            
-            int cnt_right = 1;
-            while (right - cnt_right >= 0 && arr[right - cnt_right] == arr[right]) {
-                cnt_right++;
-            }
-            
-            count += cnt_left * cnt_right;
-            left += cnt_left;
-            right -= cnt_right;  // Теперь безопасно, так как left < right
-        } 
-        else if (sum < value) {
-            left++;
-        } 
-        else {
-            right--;
-        }
+  int count = 0;
+  int end = len - 1;
+  while (end > 0) {
+    if (arr[end] > value) {
+      end--;
+    } else {
+      break;
     }
-    
-    return count;
+  }
+  for (int i = 0; i < len; i++) {
+    for (int j = end; j > i; j--) {
+      if (arr[i] + arr[j] == value)
+        count++;
+    }
+  }
+  return count;
 }
 int countPairs3(int *arr, int len, int value) {
   return 1;
