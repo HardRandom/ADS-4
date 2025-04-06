9b6b9a05bd4bc188b87acd12fa6c221cf505a714
HardRandom <90847034+HardRandom@users.noreply.github.com>
2025-04-06 18:19:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -28,6 +28,32 @@ int countPairs2(int *arr, int len, int value) {
   }
   return count;
 }
+int binarySearch(int* arr, int left, int right, int target) {
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+    if (arr[mid] == target) {
+      while (mid > left && arr[mid - 1] == target) mid--;
+      return mid;
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
 int countPairs3(int *arr, int len, int value) {
-  return 1;
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    int target = value - arr[i];
+    int count2 = binarySearch(arr, i + 1, len - 1, target);
+    if (count2 != -1) {
+      count++;
+      while (count2 + 1 < len && arr[count2] == arr[count2 + 1]) {
+        count++;
+        count2++;
+      }
+    }
+  }
+  return count;
 }
